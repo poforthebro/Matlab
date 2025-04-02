ea222ca144ac120de9e4a410351b294f6e08dcb5
Paulius Kuzmarskis <likutis2009@gmail.com>
2025-04-02 07:57:37 +0200

    attempt?

--- TNA005/AIStuff/AImain.m
+++ TNA005/AIStuff/AImain.m
@@ -1,2 +1,20 @@
 clear
-load uspsDigits.mat
+load uspsDigits
+A = testDigits(:,:,1);
+
+X_train = reshape(trainDigits,256,7291);
+y_train = trainAns;
+%X_y_comb =  
+
+X_test = reshape(testDigits,256,2007);
+y_test = testAns;
+
+y_test_1 = y_test()
+k = 1;
+
+
+%%
+
+y_pred = predict(knn_model, X_test);
+accuracy = sum(y_pred == y_test) / length(y_test) * 100;
+fprintf('Accuracy: %.2f%%\n', accuracy);
